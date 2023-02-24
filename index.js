ad2ea62332d00b182ede07fa070afc25b6c17407
Emmanuel Sarmiento <emmatombino21@gmail.com>
2023-02-24 18:09:29 -0300

    agrego formulario y localstorage

--- index.js
+++ index.js
@@ -1,6 +1,181 @@
+/**Quueria hacer un formulario de validacion de edad del cliente para que solo ingrese si es mayor,
+ * lo que no me sale es esa parte de validar ese dato de la edad. Y que una vez ingrese ahi se le ejecute el array guardado en localstorage,
+ * pero me sale el null en el JSON.parse el cual deje comentado para saber que esta mal en la corrección*/
+
+const formulario = document.getElementById("formulario");
+const inputs = document.querySelectorAll("#formulario input");
+
+const expresiones = {
+  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+  password: /^.{4,12}$/,
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  edad: /.\d {2}/,
+};
+
+const campos = {
+  nombre: false,
+  password: false,
+  correo: false,
+  edad: false,
+};
+
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "nombre":
+      validarCampo(expresiones.nombre, e.target, "nombre");
+      break;
+    case "password":
+      validarCampo(expresiones.password, e.target, "password");
+      validarPassword2();
+      break;
+    case "password2":
+      validarPassword2();
+      break;
+    case "correo":
+      validarCampo(expresiones.correo, e.target, "correo");
+      break;
+    case "edad":
+      validarCampo(expresiones.edad, e.target, "edad");
+      break;
+  }
+};
+
+const validarCampo = (expresion, input, campo) => {
+  if (expresion.test(input.value)) {
+    document
+      .getElementById(`grupo__${campo}`)
+      .classList.remove("formulario__grupo-incorrecto");
+    document
+      .getElementById(`grupo__${campo}`)
+      .classList.add("formulario__grupo-correcto");
+    document
+      .querySelector(`#grupo__${campo} i`)
+      .classList.add("fa-check-circle");
+    document
+      .querySelector(`#grupo__${campo} i`)
+      .classList.remove("fa-times-circle");
+    document
+      .querySelector(`#grupo__${campo} .formulario__input-error`)
+      .classList.remove("formulario__input-error-activo");
+    campos[campo] = true;
+  } else {
+    document
+      .getElementById(`grupo__${campo}`)
+      .classList.add("formulario__grupo-incorrecto");
+    document
+      .getElementById(`grupo__${campo}`)
+      .classList.remove("formulario__grupo-correcto");
+    document
+      .querySelector(`#grupo__${campo} i`)
+      .classList.add("fa-times-circle");
+    document
+      .querySelector(`#grupo__${campo} i`)
+      .classList.remove("fa-check-circle");
+    document
+      .querySelector(`#grupo__${campo} .formulario__input-error`)
+      .classList.add("formulario__input-error-activo");
+    campos[campo] = false;
+  }
+};
+
+const validarPassword2 = () => {
+  const inputPassword1 = document.getElementById("password");
+  const inputPassword2 = document.getElementById("password2");
+
+  if (inputPassword1.value !== inputPassword2.value) {
+    document
+      .getElementById(`grupo__password2`)
+      .classList.add("formulario__grupo-incorrecto");
+    document
+      .getElementById(`grupo__password2`)
+      .classList.remove("formulario__grupo-correcto");
+    document
+      .querySelector(`#grupo__password2 i`)
+      .classList.add("fa-times-circle");
+    document
+      .querySelector(`#grupo__password2 i`)
+      .classList.remove("fa-check-circle");
+    document
+      .querySelector(`#grupo__password2 .formulario__input-error`)
+      .classList.add("formulario__input-error-activo");
+    campos["password"] = false;
+  } else {
+    document
+      .getElementById(`grupo__password2`)
+      .classList.remove("formulario__grupo-incorrecto");
+    document
+      .getElementById(`grupo__password2`)
+      .classList.add("formulario__grupo-correcto");
+    document
+      .querySelector(`#grupo__password2 i`)
+      .classList.remove("fa-times-circle");
+    document
+      .querySelector(`#grupo__password2 i`)
+      .classList.add("fa-check-circle");
+    document
+      .querySelector(`#grupo__password2 .formulario__input-error`)
+      .classList.remove("formulario__input-error-activo");
+    campos["password"] = true;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validarFormulario);
+  input.addEventListener("blur", validarFormulario);
+});
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const terminos = document.getElementById("terminos");
+  if (campos.nombre && campos.password && terminos.checked) {
+    formulario.reset();
+
+    document
+      .getElementById("formulario__mensaje-exito")
+      .classList.add("formulario__mensaje-exito-activo");
+    setTimeout(() => {
+      document
+        .getElementById("formulario__mensaje-exito")
+        .classList.remove("formulario__mensaje-exito-activo");
+    }, 5000);
+
+    document
+      .querySelectorAll(".formulario__grupo-correcto")
+      .forEach((icono) => {
+        icono.classList.remove("formulario__grupo-correcto");
+      });
+  } else {
+    document
+      .getElementById("formulario__mensaje")
+      .classList.add("formulario__mensaje-activo");
+  }
+});
+
+const calcularEdad = (fechaNacimiento) => {
+  const fechaActual = new Date();
+  const anoActual = parseInt(fhecaActual.getFullYear());
+  const mesActual = parseInt(fhecaActual.getMonth()) + 1;
+  const diaActual = parseInt(fhecaActual.getDate());
+
+  const anoNacimiento = parseInt(String(fechaNacimiento).substring(0, 4));
+  const mesNacimiento = parseInt(String(fechaNacimiento).substring(5, 7));
+  const diaNacimiento = parseInt(String(fechaNacimiento).substring(8, 10));
+
+  let edad = anoActual - anoNacimiento;
+  if (mesActual < mesNacimiento) {
+    edad--;
+  } else if (mesActual === mesNacimiento) {
+    if (diaActual < diaNacimiento) {
+      edad--;
+    }
+  }
+  return edad;
+};
+
 let carrito = [];
 
-let bebidasArrays = [
+let cervezasArrays = [
   {
     id: 1,
     marca: "Quilmes",
@@ -165,33 +340,44 @@ let bebidasArrays = [
   },
 ];
 
-let opciones = Number(
-  prompt(`Elija una bebida segun una opción disponible: 
-                    1- ${bebidasArrays[0].marca} - ${bebidasArrays[0].sabor} - ${bebidasArrays[0].envase}
-                    2- ${bebidasArrays[1].marca} - ${bebidasArrays[1].sabor} - ${bebidasArrays[1].envase}
-                    3- ${bebidasArrays[2].marca} - ${bebidasArrays[2].sabor} - ${bebidasArrays[2].envase}
-                    4- ${bebidasArrays[3].marca} - ${bebidasArrays[3].sabor} - ${bebidasArrays[3].envase}
-                    5- ${bebidasArrays[4].marca} - ${bebidasArrays[4].sabor} - ${bebidasArrays[4].envase}
-                    6- ${bebidasArrays[5].marca} - ${bebidasArrays[5].sabor} - ${bebidasArrays[5].envase}
-                    7- ${bebidasArrays[6].marca} - ${bebidasArrays[6].sabor} - ${bebidasArrays[6].envase}
-                    8- ${bebidasArrays[7].marca} - ${bebidasArrays[7].sabor} - ${bebidasArrays[7].envase}
-                    9- ${bebidasArrays[8].marca} - ${bebidasArrays[8].sabor} - ${bebidasArrays[8].envase}
-                    10- ${bebidasArrays[9].marca} - ${bebidasArrays[9].sabor} - ${bebidasArrays[9].envase}
-                    11- ${bebidasArrays[10].marca} - ${bebidasArrays[10].sabor} - ${bebidasArrays[10].envase}
-                    12- ${bebidasArrays[11].marca} - ${bebidasArrays[11].sabor} - ${bebidasArrays[11].envase}
-                    13- ${bebidasArrays[12].marca} - ${bebidasArrays[12].sabor} - ${bebidasArrays[12].envase}
-                    14- ${bebidasArrays[13].marca} - ${bebidasArrays[13].sabor} - ${bebidasArrays[13].envase}
-                    15- ${bebidasArrays[14].marca} - ${bebidasArrays[14].sabor} - ${bebidasArrays[14].envase}
-                    16- ${bebidasArrays[15].marca} - ${bebidasArrays[15].sabor} - ${bebidasArrays[15].envase}
-                    17- ${bebidasArrays[16].marca} - ${bebidasArrays[16].sabor} - ${bebidasArrays[16].envase}
-                    18- ${bebidasArrays[17].marca} - ${bebidasArrays[17].sabor} - ${bebidasArrays[17].envase}
-                  `)
-);
+const cervezasJSON = JSON.stringify(cervezasArrays);
+
+localStorage.setItem("cervezas", cervezasJSON);
+
+console.log(cervezasJSON);
+
+// const cervezaJSON = localStorage.getItem("cervezasArrays");
+
+// const cervezas = JSON.parse(cervezaJSON);
+
+// console.log(cervezas);
+
+let opciones = Number();
+// prompt(`Elija una bebida segun una opción disponible:
+//                   1- ${cervezasArrays[0].marca} - ${cervezasArrays[0].sabor} - ${cervezasArrays[0].envase}
+//                   2- ${cervezasArrays[1].marca} - ${cervezasArrays[1].sabor} - ${cervezasArrays[1].envase}
+//                   3- ${cervezasArrays[2].marca} - ${cervezasArrays[2].sabor} - ${cervezasArrays[2].envase}
+//                   4- ${cervezasArrays[3].marca} - ${cervezasArrays[3].sabor} - ${cervezasArrays[3].envase}
+//                   5- ${cervezasArrays[4].marca} - ${cervezasArrays[4].sabor} - ${cervezasArrays[4].envase}
+//                   6- ${cervezasArrays[5].marca} - ${cervezasArrays[5].sabor} - ${cervezasArrays[5].envase}
+//                   7- ${cervezasArrays[6].marca} - ${cervezasArrays[6].sabor} - ${cervezasArrays[6].envase}
+//                   8- ${cervezasArrays[7].marca} - ${cervezasArrays[7].sabor} - ${cervezasArrays[7].envase}
+//                   9- ${cervezasArrays[8].marca} - ${cervezasArrays[8].sabor} - ${cervezasArrays[8].envase}
+//                   10- ${cervezasArrays[9].marca} - ${cervezasArrays[9].sabor} - ${cervezasArrays[9].envase}
+//                   11- ${cervezasArrays[10].marca} - ${cervezasArrays[10].sabor} - ${cervezasArrays[10].envase}
+//                   12- ${cervezasArrays[11].marca} - ${cervezasArrays[11].sabor} - ${cervezasArrays[11].envase}
+//                   13- ${cervezasArrays[12].marca} - ${cervezasArrays[12].sabor} - ${cervezasArrays[12].envase}
+//                   14- ${cervezasArrays[13].marca} - ${cervezasArrays[13].sabor} - ${cervezasArrays[13].envase}
+//                   15- ${cervezasArrays[14].marca} - ${cervezasArrays[14].sabor} - ${cervezasArrays[14].envase}
+//                   16- ${cervezasArrays[15].marca} - ${cervezasArrays[15].sabor} - ${cervezasArrays[15].envase}
+//                   17- ${cervezasArrays[16].marca} - ${cervezasArrays[16].sabor} - ${cervezasArrays[16].envase}
+//                   18- ${cervezasArrays[17].marca} - ${cervezasArrays[17].sabor} - ${cervezasArrays[17].envase}
+//                 `)
 
 if (opciones >= 1 && opciones <= 17) {
-  carrito.push(bebidasArrays.find((bebida) => bebida.id === opciones));
+  carrito.push(cervezasArrays.find((bebida) => bebida.id === opciones));
   carritoActualizado();
-  alert(JSON.stringify(carrito));
+  // alert(JSON.stringify(carrito));
 }
 
 function carritoActualizado() {
